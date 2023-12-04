13d3c9359351486bc6c4e876aab8386596b55dbf
gushchinda <gushchinda@autoins.ru>
2023-12-04 21:45:31 +0300

    отправка почты

--- src/templates/ContactForm.js
+++ src/templates/ContactForm.js
@@ -1,8 +1,20 @@
 import axios from 'axios'
 
 function ContactForm() {
-    const sendEmail = ()=>{
-        axios.get("http://localhost:8081")
+    const sendEmail = (e)=>{
+        e.preventDefault();
+        const data = JSON.stringify({
+            serviceNeed:document.getElementById('serviceNeed').value,
+            name:document.getElementById('name').value,
+            email:document.getElementById('email').value,
+            phone:document.getElementById('phone').value
+        })
+        const options = {
+            headers:{
+                "Content-Type":"application/json"
+            }
+        }
+        axios.post("http://localhost:8081/mail",data,options)
             .then(response => console.log("response", response.data))
             .catch(error => console.log(error))
     }
@@ -21,7 +33,7 @@ function ContactForm() {
             <input className="contactForm-input" type="email" name="email" id="email" required/>
             <label className="inputLabel" htmlFor="phone">Phone:</label>
             <input className="contactForm-input" type="phone" name="phone" id="phone" required/>
-            <button type="submit" className="contactForm-submit" onClick={sendEmail}>Submit</button>
+            <button type="submit" className="contactForm-submit" onClick={(e)=>sendEmail(e)}>Submit</button>
         </form>
     )
 }
